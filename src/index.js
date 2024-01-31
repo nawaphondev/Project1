600b2e7dc5e4a893d37a326d04cd305928c84e1b
nawaphondev <nawaphonphophom55@gmail.com>
2024-01-31 17:56:49 +0700

    Express

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const prisma = new PrismaClient();
+const errorHandler = require("./middlewares/error");
+const notFoundHandler = require("./middlewares/notFound");
+const createError = require("./utils/createError");
+
+const authRoute = require("./routes/auth-route");
+const authenticate = require("./middlewares/authenticate");
+
+
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoute);
+app.use("/auth", authenticate);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.use(errorHandler);
+app.use("*", notFoundHandler);
+
+const port = process.env.PORT;
+if (!port) {
+  console.error("Port is not set in the environment variables.");
+  process.exit(1);
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
---
+++ src/middlewares/notFound.js
@@ -0,0 +1,6 @@
+const notFoundHandler = (req, res, next) => {
+    res.status(404).json({ message: "Not Found" });
+  };
+  
+  module.exports = notFoundHandler;
+  
